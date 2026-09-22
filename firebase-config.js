8df0a9eeb3ab314a76e27067ab14718c09e48004
MrUnknow31 <299318552+MrUnknow31@users.noreply.github.com>
2026-09-22 12:00:01 +0200

    Firebase-Konfiguration eintragen

--- firebase-config.js
+++ firebase-config.js
@@ -5,10 +5,10 @@
 // Die Platzhalter unten ersetzen, speichern und hochladen.
 // ============================================================
 export const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "DEINE_SENDER_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyBHWi-eo2CaxQEpp6Pk6AjNf3elSm1bZfI",
+  authDomain: "raphdi-familienseite.firebaseapp.com",
+  projectId: "raphdi-familienseite",
+  storageBucket: "raphdi-familienseite.firebasestorage.app",
+  messagingSenderId: "401665237605",
+  appId: "1:401665237605:web:70d5c3fa5652671571dc8c"
 };
